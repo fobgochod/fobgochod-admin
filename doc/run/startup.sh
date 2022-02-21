bc61554a46326014d9c0dfb74c181d26e83bbf3f
zhouxiao <764800230@qq.com>
2022-02-22 01:57:23 +0800

    update

--- doc/run/startup.sh
+++ doc/run/startup.sh
@@ -5,5 +5,5 @@ docker run -idt --restart=always --privileged=true --name fobgochod-admin \
   -e java_opts='-Xms128m -Xmx300m' \
   -e profile_active='dev' \
   -e mongodb_uri='mongodb://172.19.215.14:27017/admin' \
-  -e base_uri='http://zhouxiao.co' \
+  -e base_uri='http://admin.zhouxiao.co' \
   fobgochod/fobgochod-admin:1.0.0
